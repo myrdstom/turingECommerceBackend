d06388e75dddef43cac91feca859ab4dc6ec83ce
myrdstom <nserekopaul@gmail.com>
2019-09-18 18:03:42 +0300

    Connecting to the database

--- src/database/models/attribute.js
+++ src/database/models/attribute.js
@@ -1,31 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-  const Attribute = sequelize.define(
-    'Attribute',
-    {
-      attribute_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        validate: {
-          notEmpty: true,
+    const Attribute = sequelize.define(
+        'Attribute',
+        {
+            attribute_id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            name: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+            },
         },
-      },
-    },
-    {
-      timestamps: false,
-      tableName: 'attribute',
-    }
-  );
+        {
+            timestamps: false,
+            tableName: 'attribute',
+        }
+    );
 
-  Attribute.associate = ({ AttributeValue }) => {
-    Attribute.hasMany(AttributeValue, {
-      foreignKey: 'attribute_id',
-    });
-  };
+    Attribute.associate = ({ AttributeValue }) => {
+        Attribute.hasMany(AttributeValue, {
+            foreignKey: 'attribute_id',
+        });
+    };
 
-  return Attribute;
+    return Attribute;
 };
